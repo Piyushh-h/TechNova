5924f1a351d0a9e56f68c5d2cdd3db8df737c069
Piyushh-h <piyushsinha0810@gmail.com>
2025-01-28 18:57:56 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/TechNova/',  // Add this line
-  // ... rest of your config
-})
+  plugins: [react()],
+});
